3a3be139264c6b4d4f3377b21228dc3f4ec9db3a
AshutoshSundresh <ashutoshsundresh@gmail.com>
2020-12-01 21:14:19 +0530

    davinci: Hide SELinux status in settings

--- device.mk
+++ device.mk
@@ -190,7 +190,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-ssos
 
 # VINTF
 PRODUCT_COPY_FILES += \
