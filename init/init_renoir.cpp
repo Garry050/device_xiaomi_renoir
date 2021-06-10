220334949135a03aa8aea58d03bbec1da08735e4
AdarshGrewal <adarsh@aosip.dev>
2021-06-10 20:55:12 +0530

    renoir: init: SafetyNet workaround
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- init/init_renoir.cpp
+++ init/init_renoir.cpp
@@ -101,6 +101,12 @@ void vendor_load_properties() {
 	mod_device = "renoir_jp_global";
     }
 
+    // SafetyNet workaround
+    property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.boot.flash.locked", "1");
+    fingerprint = "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys";
+    description = "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys";
+
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
     set_ro_product_prop("model", model);
