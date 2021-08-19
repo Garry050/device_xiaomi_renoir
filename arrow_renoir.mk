77dff2bff6f0ec21182eb7136a996fa29defe56d
VladV1V <mahno444@gmail.com>
2021-08-19 16:06:26 +0300

    fixes

--- arrow_renoir.mk
+++ arrow_renoir.mk
@@ -45,8 +45,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build info
-BUILD_FINGERPRINT := "Redmi/renoir/renoir:11/RKQ1.201112.002/V12.5.3.0.RKIEUXM:user/release-keys"
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=renoir \
-    PRODUCT_NAME=renoir \
-    PRIVATE_BUILD_DESC="renoir-user 11 RKQ1.201112.002 V12.5.3.0.RKIEUXM release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+
--- device.mk
+++ device.mk
@@ -109,6 +109,10 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
+# HotwordEnrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Live Wallpapers
 PRODUCT_PACKAGES += \
     LiveWallpapers \
@@ -186,3 +190,5 @@ include vendor/qcom/opensource/display-commonsys-intf/config/display-product-sys
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=log
+
+include vendor/xiaomi/renoir/renoir-vendor.mk
