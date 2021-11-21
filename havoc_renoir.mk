03830687f19e60ae2f2fcece03aed5e934db34ec
Garry050 <garry.windows8@gmail.com>
2021-11-21 13:28:25 +0900

    renoir: include GApps

--- havoc_renoir.mk
+++ havoc_renoir.mk
@@ -14,6 +14,13 @@ $(call inherit-product, device/xiaomi/renoir/device.mk)
 # Inherit some common Havoc-OS stuff.
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
+# Setup Gapps options
+IS_PHONE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
