accf49f8ad63a8b2b61d3d68969463240e0283a0
Garry050 <garry.windows8@gmail.com>
2021-11-21 13:24:43 +0900

    renoir: bringup Havoc-OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_renoir.mk
+    $(LOCAL_DIR)/havoc_renoir.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_renoir-user \
-    dot_renoir-userdebug \
-    dot_renoir-eng
+    havoc_renoir-user \
+    havoc_renoir-userdebug \
+    havoc_renoir-eng
--- havoc_renoir.mk
+++ havoc_renoir.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The DotOS Project
+# Copyright (C) 2021 The Havoc-OS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,20 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common DotOS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
+# Inherit some common Havoc-OS stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 5G
 PRODUCT_NAME := dot_renoir
-
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_BLUR := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 WITH_GAPPS := true
